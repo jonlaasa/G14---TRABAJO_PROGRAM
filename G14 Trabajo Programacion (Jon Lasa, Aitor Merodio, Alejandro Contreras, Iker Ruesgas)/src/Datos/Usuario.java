6b370e5a6da5546008f22c48020aa06c41715189
alexc <alexc@10.164.48.225>
2022-10-04 12:54:15 +0200

    prueba

--- G14 Trabajo Programacion (Jon Lasa, Aitor Merodio, Alejandro Contreras, Iker Ruesgas)/src/Datos/Usuario.java
+++ G14 Trabajo Programacion (Jon Lasa, Aitor Merodio, Alejandro Contreras, Iker Ruesgas)/src/Datos/Usuario.java
@@ -3,5 +3,24 @@ package Datos;
 public class Usuario {
 	
 	private String nombre;
+	
+	private String dni;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
 
 }
