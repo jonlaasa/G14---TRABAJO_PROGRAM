0288cc3f708d4318e35d48e704a091b30ed014a4
jontx <jontx@DESKTOP-2RVMOHR>
2022-11-05 22:18:14 +0100

    Metodos en utils

--- G14 Trabajo Programacion (Jon Lasa, Aitor Merodio, Alejandro Contreras, Iker Ruesgas)/src/Logica_de_Negocio/Utils.java
+++ G14 Trabajo Programacion (Jon Lasa, Aitor Merodio, Alejandro Contreras, Iker Ruesgas)/src/Logica_de_Negocio/Utils.java
@@ -26,18 +26,84 @@ public static boolean comprobarMail(String a) throws CorreoNoValidoException {
 	    	  return true;
 	      }
 	      else {
-	    	  throw new CorreoNoValidoException();
+	    	  throw new CorreoNoValidoException("Correo incorrecto: " + a);
 	    	  
 	      }
 	}
 
 
-//METODO PARA COMPROBAR SI EL VALOR INTRODUCIDO ES UN NUMERO
+//METODO PARA COMPROBAR SI EL PRECIO VALOR INTRODUCIDO ES UN VALIDO
 
-	public static boolean comprobarNumero(String a) throws IllegalArgumentException{
+	public static boolean comprobarPrecio(String a) throws PrecioException{
 		try {
 			int numero =Integer.parseInt(a);
+			if (numero>=0) {
+				return true;
+			}
+			else {
+				throw new PrecioException("Precio negativo: " + numero);
+			}
+				
+			
+		}catch(IllegalArgumentException b) {
+			throw new IllegalArgumentException();
+		}
+		
+		
+	}
+	
+	
+	public static boolean comprobarDuracion(String a) throws DuracionException{
+		try {
+			int numero =Integer.parseInt(a);
+			if (numero<=0) {
+				throw new DuracionException("Duracion de 0 o negativo NO VALIDO : " + numero);
+			}
 			return true;
+		}catch(IllegalArgumentException b) {
+			throw new IllegalArgumentException();
+	}
+	}
+	
+	
+	
+	
+	public static boolean comprobarPreciPlazas(String a) throws PlazasRestantesException{
+		try {
+			int numero =Integer.parseInt(a);
+			if (numero>=0) {
+				return true;
+			}
+			else {
+				throw new PlazasRestantesException("Plazas negativo: " + numero);
+			}
+				
+			
+		}catch(IllegalArgumentException b) {
+			throw new IllegalArgumentException();
+		}
+		
+		
+	}
+	
+	
+	public static boolean comprobarStringUsuario(String a) throws LongitudStringException{
+		try {
+			if(a.length()<=6) {
+				throw new LongitudStringException("Longitud no VALIDA (TIENE QUE SER MAYOR QUE 6 : " + a);
+			}
+			char[] palabra = a.toCharArray();
+			if(!Character.isLetter(palabra[0])) {
+				
+				throw new LongitudStringException("Primer caracter no valido, tiene que empezar por letra : " + a);
+			}
+			
+			
+			else {
+				return true;
+			}
+				
+			
 		}catch(IllegalArgumentException b) {
 			throw new IllegalArgumentException();
 		}
@@ -49,5 +115,9 @@ public static boolean comprobarMail(String a) throws CorreoNoValidoException {
 	
 	
 	
+	
+	
+	
+
 
 }
